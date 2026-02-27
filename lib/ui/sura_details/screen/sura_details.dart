59ba7f06b1115a51f899f7c1e1a629926cce4635
Said Elhadidi <saidelhadidi1@gmail.com>
2026-02-27 13:59:23 +0200

    feat(quran): integrate sura text assets and implement dynamic Text.rich for verse styling

--- lib/ui/sura_details/screen/sura_details.dart
+++ lib/ui/sura_details/screen/sura_details.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami_app/core/resources/assets_manager.dart';
 import 'package:islami_app/core/resources/colors_manager.dart';
 import 'package:islami_app/model/sura_model.dart';
 
-class SuraDetails extends StatelessWidget {
+class SuraDetails extends StatefulWidget {
   const SuraDetails({super.key});
 
   static const String routeName = "sura_details";
 
   @override
-  Widget build(BuildContext context) {
+  State<SuraDetails> createState() => _SuraDetailsState();
+}
 
+class _SuraDetailsState extends State<SuraDetails> {
+  List<String> verses = [];
+  Future<void> readFile(int suraNumber) async {
+    String sureTextFile = await rootBundle.loadString(
+      "assets/suras/$suraNumber.txt",
+    );
+    verses = sureTextFile.split("\n");
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     SuraModel suraModel =
         ModalRoute.of(context)!.settings.arguments as SuraModel;
-
+    if (verses.isEmpty) {
+      readFile(suraModel.number);
+    }
     return Scaffold(
       backgroundColor: ColorsManager.brown,
       appBar: AppBar(
@@ -24,45 +40,75 @@ class SuraDetails extends StatelessWidget {
         elevation: 0,
         title: Text(
           suraModel.eName,
-          style: TextStyle(color: ColorsManager.gold,fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: ColorsManager.gold,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
       body: Column(
         children: [
-         Expanded(
-           child: Padding(
-             padding: const EdgeInsets.all(10),
-             child: Column(
-               children: [
-                 Row(
-                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                   children: [
-                     Image.asset(AssetsManager.leftCorner),
-                     Text(
-                       suraModel.aName,
-                       style: TextStyle(color: ColorsManager.gold, fontSize: 26,fontWeight: FontWeight.bold),
-                     ),
-                     Image.asset(AssetsManager.rightCorner),
-                   ],
-                 ),
-                 Expanded(
-                   child: SingleChildScrollView(
-                     child: Text(
-                       "يَا أَيُّهَا النَّاسُ اتَّقُوا رَبَّكُمُ الَّذِي خَلَقَكُم مِّن نَّفْسٍ وَاحِدَةٍ وَخَلَقَ مِنْهَا زَوْجَهَا وَبَثَّ مِنْهُمَا رِجَالًا كَثِيرًا وَنِسَاءً ۚ وَاتَّقُوا اللَّهَ الَّذِي تَسَاءَلُونَ بِهِ وَالْأَرْحَامَ ۚ إِنَّ اللَّهَ كَانَ عَلَيْكُمْ رَقِيبًا (1) وَآتُوا الْيَتَامَىٰ أَمْوَالَهُمْ ۖ وَلَا تَتَبَدَّلُوا الْخَبِيثَ بِالطَّيِّبِ ۖ وَلَا تَأْكُلُوا أَمْوَالَهُمْ إِلَىٰ أَمْوَالِكُمْ ۚ إِنَّهُ كَانَ حُوبًا كَبِيرًا (2) وَإِنْ خِفْتُمْ أَلَّا تُقْسِطُوا فِي الْيَتَامَىٰ فَانكِحُوا مَا طَابَ لَكُم مِّنَ النِّسَاءِ مَثْنَىٰ وَثُلَاثَ وَرُبَاعَ ۖ فَإِنْ خِفْتُمْ أَلَّا تَعْدِلُوا فَوَاحِدَةً أَوْ مَا مَلَكَتْ أَيْمَانُكُمْ ۚ ذَٰلِكَ أَدْنَىٰ أَلَّا تَعُولُوا (3) وَآتُوا النِّسَاءَ صَدُقَاتِهِنَّ نِحْلَةً ۚ فَإِن طِبْنَ لَكُمْ عَن شَيْءٍ مِّنْهُ نَفْسًا فَكُلُوهُ هَنِيئًا مَّرِيئًا (4) وَلَا تُؤْتُوا السُّفَهَاءَ أَمْوَالَكُمُ الَّتِي جَعَلَ اللَّهُ لَكُمْ قِيَامًا وَارْزُقُوهُمْ فِيهَا وَاكْسُوهُمْ وَقُولُوا لَهُمْ قَوْلًا مَّعْرُوفًا (5)",
-                       textDirection: TextDirection.rtl,
-                       textAlign: TextAlign.center,
-                       style: TextStyle(
-                         height: 2.5,
-                         color: ColorsManager.gold,
-                         fontSize: 24,
-                       ),
-                     ),
-                   ),
-                 ),
-               ],
-             ),
-           ),
-         ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(AssetsManager.leftCorner),
+                      Text(
+                        suraModel.aName,
+                        style: TextStyle(
+                          color: ColorsManager.gold,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Image.asset(AssetsManager.rightCorner),
+                    ],
+                  ),
+                  Expanded(
+                    child: verses.isEmpty
+                        ? Center(
+                      child: CircularProgressIndicator(
+                        color: ColorsManager.gold,
+                      ),
+                    )
+                        : SingleChildScrollView(
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            for (int i = 0; i < verses.length; i++) ...[
+                              TextSpan(
+                                text: verses[i],
+                                style: TextStyle(
+                                  color: ColorsManager.gold,
+                                  fontSize: 24,
+                                  height: 2.5,
+                                ),
+                              ),
+                              TextSpan(
+                                text: "[${i + 1}] ",
+                                style: TextStyle(
+                                  color: ColorsManager.offWhite,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ]
+                          ],
+                        ),
+                        textDirection: TextDirection.rtl,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                  ),
+                ],
+              ),
+            ),
+          ),
           Image.asset(AssetsManager.bottom),
         ],
       ),
